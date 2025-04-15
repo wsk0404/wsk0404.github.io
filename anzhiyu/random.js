87c5b49d98a5c59a1a8d7d97a9fe71fe89e2a9e1
gukeyang <1656473414@qq.com>
2025-04-15 17:34:13 +0800

    Site updated: 2025-04-15 17:34:12

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/4a17b152.html","posts/4a17b153.html","posts/4a17b157.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/4a17b152.html","posts/4a17b153.html","posts/undefined.html","posts/4a17b157.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
